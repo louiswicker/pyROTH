f01f3288b7cafa5c86393962df1b04eb5db66b53
Louis.Wicker <Louis.Wicker@noaa.gov>
2017-03-30 22:42:37 -0500

    Fix a bug in the spec of which day it is (use time, not datetime), cause of the local versus GMT thing

--- prep_grid3d_catchup.py
+++ prep_grid3d_catchup.py
@@ -8,19 +8,19 @@ _MRMS_obs_seq   = "/work/john.krause/realtime"
 
 year = 2017
 mon  = 03
-day  = 30
-hour = 18
+day  = 31
+hour = 03
 dt        = datetime.datetime(year, mon, day, hour, 0, 0)
-stop_time = datetime.datetime(year, mon, day, 21, 45, 0)
+stop_time = datetime.datetime(year, mon, day, 03, 31, 0)
 dtime     = datetime.timedelta(minutes=15)
 
 day       = datetime.datetime(year, mon, day, 0, 0)
 
-obs_seq_out_dir = os.path.join(_MRMS_obs_seq, day.strftime("%Y%m%d"))
+obs_seq_out_dir = os.path.join(_MRMS_obs_seq, time.strftime("%Y%m%d"))
 
 while dt < stop_time:
 
-    cmd = "prep_grid3d.py -d %s -w -o %s --realtime %s -p 2" % (_MRMS_input_directory, obs_seq_out_dir, dt.strftime("%Y%m%d%H%M"))
+    cmd = "prep_grid3d.py -d %s -w -o %s --realtime %s -p 4" % (_MRMS_input_directory, obs_seq_out_dir, dt.strftime("%Y%m%d%H%M"))
     print("\n Run_MRMS running job: at %s" % (time.strftime("%Y-%m-%d %H:%M:%S")))
     print(" Command: %s\n" % (cmd))
     os.system("%s >> log_prep_grid3d" % cmd)
